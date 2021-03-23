7ad8beb62b275cce0ec07991d5104f281c524ec7
Lucas Petersen <ima_lucas@hotmail.com>
2021-03-23 21:59:01 +1100

    Utilities script

---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities
+{
+    ////////////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////////////
+    // Mouse Position
+    public static Vector3 GetMousePosition()
+    {
+        Vector3 vec = GetMousePositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0;
+        return vec;
+    }
+
+    public static Vector3 GetMousePositionWithZ()
+    {
+        return GetMousePositionWithZ(Input.mousePosition, Camera.main);
+    }
+
+    public static Vector3 GetMousePositionWithZ(Camera worldCamera)
+    {
+        return GetMousePositionWithZ(Input.mousePosition, worldCamera);
+    }
+
+    public static Vector3 GetMousePositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+}
