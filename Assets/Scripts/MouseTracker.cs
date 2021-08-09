0182ca45abe8faab2eb8b35ab692bdd10468a61d
Lucas Petersen <ima_lucas@hotmail.com>
2021-08-10 02:15:50 +1000

    Using Unity's SpriteShape to draw various polygons
    This is also how the 2D colliders are generated and therefore how mouse detection is being found

---
+++ Assets/Scripts/MouseTracker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.U2D;
+
+public class MouseTracker : MonoBehaviour
+{
+    public Color highlightColour = Color.white;
+
+    [Header("Mouse Events")]
+    public UnityEvent Enter;
+    public UnityEvent Stay;
+    public UnityEvent Exit;
+
+    SpriteShapeRenderer m_spriteRenderer;
+
+    Color m_defaultColour;
+
+    private void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteShapeRenderer>();
+        m_defaultColour = m_spriteRenderer.color;
+    }
+
+    private void OnMouseEnter()
+    {
+        Enter.Invoke();
+        m_spriteRenderer.color = highlightColour;
+    }
+
+    private void OnMouseOver()
+    {
+        Stay.Invoke();
+    }
+
+    private void OnMouseExit()
+    {
+        Exit.Invoke();
+        m_spriteRenderer.color = m_defaultColour;
+    }
+}
